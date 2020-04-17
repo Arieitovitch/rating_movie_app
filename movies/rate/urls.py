01d25989dc7dce498620213afb55c44ac357f684
Arieitovitch <59893909+Arieitovitch@users.noreply.github.com>
2020-04-17 13:08:48 -0400

    yo

--- movies/rate/urls.py
+++ movies/rate/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.signin, name='signin'),
-    path('sigin', views.signin, name='signin'),
+    path('signin', views.signin, name='signin'),
     path('signup', views.signup, name='signup'),
     path('home', views.home, name='home'),
     path('ratings', views.ratings, name='ratings'),
